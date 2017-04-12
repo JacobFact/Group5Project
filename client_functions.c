ddc076ed2ba2b63753e3845e33af79896802e740
AshWije <ashiniwije126@gmail.com>
2017-04-12 17:54:42 -0500

    Update client_functions.c

--- client_functions.c
+++ client_functions.c
@@ -1,3 +1,9 @@
+// client_functions.c
+// contains the common functions of the client.c file
+
+#include <std.io>
+
+// the error function prints the error message that is passed as an argument to it
 void error(const char *msg)
 {
     perror(msg);
