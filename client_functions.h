430cf9b0d851c0de81a2315fc1e5c0d3228be6f5
AshWije <ashiniwije126@gmail.com>
2017-04-12 18:02:47 -0500

    Update client_functions.h

--- client_functions.h
+++ client_functions.h
@@ -1,4 +1,5 @@
 // client_functions.h
+// header file that contains the declarations of the files that are declared in the client_functions.c file
 
-// error function that is declared in the client_functions.c file
+// declaration of the error function
 void error(const char *);
