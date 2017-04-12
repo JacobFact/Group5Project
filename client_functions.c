75587573922d99e57526b69394dcf6eabaaac757
AshWije <ashiniwije126@gmail.com>
2017-04-12 18:05:53 -0500

    Update client_functions.c

--- client_functions.c
+++ client_functions.c
@@ -2,7 +2,6 @@
 // contains the common functions of the client.c file
 
 #include <std.io>
-#include "client_functions.h"
 
 // the error function prints the error message that is passed as an argument to it
 void error(const char *msg)
