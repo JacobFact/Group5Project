f2f20d4e153a8820792fe7bd019cba7b19e3e164
AnvithaSag <anvithasag@gmail.com>
2017-04-12 17:57:44 -0500

    Update server.c

--- server.c
+++ server.c
@@ -7,11 +7,6 @@
 #include <sys/un.h>
 #include <stdio.h>
 #include "server_files.h"
-//void error(const char *);
-/*void *SigCatcher(int n)
-{
-  wait3(NULL,WNOHANG,NULL);
-}*/
 int main(int argc, char *argv[])
 {
    int sockfd, newsockfd, servlen, n;
@@ -61,28 +56,3 @@ int main(int argc, char *argv[])
  for each connection.  It handles all communication
  once a connnection has been established.
  *****************************************/
-/*void dostuff (int sock)
-{
-   int n;
-   char buffer[256];
-      
-   //read from client
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0){
-      error("ERROR reading from socket");
-   }
-   //Display the message
-   printf("Here is the message: %s\n",buffer);
-   //Reply
-   n = write(sock,"I got your message",18);
-   if (n < 0){
-       error("ERROR writing to socket");
-   }
-}
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}*/
