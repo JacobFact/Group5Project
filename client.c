c774e9db56e08f5c8d41223b1bb637e3cda0a921
JacobFact <42regime@gmail.com>
2017-04-12 11:18:07 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,57 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+/* a client in the unix domain */
 #include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
+#include <sys/un.h>
+#include <stdio.h>
+void error(const char *);
+
+int main(int argc, char *argv[])
+{
+   int sockfd, servlen,n;
+   struct sockaddr_un  serv_addr;
+   char buffer[82];
+
+   bzero((char *)&serv_addr,sizeof(serv_addr));
+   serv_addr.sun_family = AF_UNIX;
+   strcpy(serv_addr.sun_path, argv[1]);
+   servlen = strlen(serv_addr.sun_path) + 
+                 sizeof(serv_addr.sun_family);
+   if ((sockfd = socket(AF_UNIX, SOCK_STREAM,0)) < 0)
+       error("Creating socket");
+   if (connect(sockfd, (struct sockaddr *) 
+                         &serv_addr, servlen) < 0)
+       error("Connecting");
+   printf("Please enter your message: ");
+   bzero(buffer,82);
+   fgets(buffer,80,stdin);
+   write(sockfd,buffer,strlen(buffer));
+   n=read(sockfd,buffer,80);
+   printf("The return message was\n");
+   write(1,buffer,n);
+   close(sockfd);
+   return 0;
+}
 
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
 }
