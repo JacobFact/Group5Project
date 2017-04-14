2c5bea40d35fc3c4172daa49f73aeffba571f5c2
JacobFact <42regime@gmail.com>
2017-04-13 20:12:10 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,58 +1,58 @@
-/* a client in the unix domain */
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/un.h>
+//A client in the internet domain
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
-#include "client_functions.h"
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
 int main(int argc, char *argv[])
 {
-   //sockfd: stores socket system call values|servlen: stores the size of the address of the server|n: stores the return values of read() and write() calls
-   int sockfd, servlen,n;
-   //sockaddr_un: structure containing an port address for TCP and/or UCP|serv_addr: contains the address of the server
-   struct sockaddr_un  serv_addr;
-   //Server reads character from the socket connection into this buffer
-   char buffer[82];
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-   //Sets all values in the buffer to zero
-   bzero((char *)&serv_addr,sizeof(serv_addr));
-   
-   //Contains code for the address family, UNIX domain
-   serv_addr.sun_family = AF_UNIX;
-   
-   //Copies the string (port number(?)) from the command line to serv_addr.sun_path
-   strcpy(serv_addr.sun_path, argv[1]);
-   
-   //Stores the server address
-   servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
-   
-   //creates a new socket with the Unix address domain, stream type, and protocal message
-   if ((sockfd = socket(AF_UNIX, SOCK_STREAM,0)) < 0){
-      //Error message if socket call fails
-      error("Socket Call Fail: Exiting...");
-   }
-   
-   //connect to server
-   if (connect(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0){
-      //Error Message
-      error("Connection Failed: Exiting...");
-   }
-   
-   //Message input
-   printf("Please enter your message: ");
-   bzero(buffer,82);
-   //Reads from stdin
-   fgets(buffer,80,stdin);
-   //Writes message to socket
-   write(sockfd,buffer,strlen(buffer));
-   //Reads reply from the socket
-   n=read(sockfd,buffer,80);
-   //Display
-   printf("The return message was\n");
-   write(1,buffer,n);
-   close(sockfd);
-   return 0;
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
 }
