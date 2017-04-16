b1bddd815341d266ba94b4abc72a3dd32e930542
AshWije <ashiniwije126@gmail.com>
2017-04-16 18:59:06 -0500

    Update client_functions.c

--- client_functions.c
+++ client_functions.c
@@ -2,10 +2,32 @@
 // contains the common functions of the client.c file
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <arpa/inet.h>
 
-// the error function prints the error message that is passed as an argument to it
+// prints the error message that is passed as an argument
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
+}
+
+// creates a socket of the type specified
+int makeSocket(int type)
+{
+    // creates a new socket
+    sockfd = socket(AF_INET, type, 0);
+        
+    // outputs an error if the socket creation failed
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    
+    // return the socket
+    return sockfd;
 }
