1d789f47d725cc56e6607ba4745a3df148ad594e
AnvithaSag <anvithasag@gmail.com>
2017-04-16 08:23:40 -0500

    Update server.c

--- server.c
+++ server.c
@@ -32,6 +32,17 @@ int main(int argc, char *argv[])
               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0)
+      error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) 
+        error("ERROR reading from socket");
+     printf("Here is the message: %s",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) 
+        error("ERROR writing to socket");
      //Causes a nonblocking wait to prevent Zombie processes
      signal(SIGCHLD,SigCatcher);
      while (1) {
