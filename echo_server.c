2f21d9331ba610bfe662872238f9837023fc950c
axb157030 <amine.benelbar.1@gmail.com>
2017-04-21 15:55:10 -0500

    Update echo_server.c
    I think multiple port numbers

--- echo_server.c
+++ echo_server.c
@@ -23,14 +23,27 @@ int main(int argc, char *argv[])
 
 
    int sockfd, newsockfd, portno, sockfd_udp, length, n;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     socklen_t fromlen;
-     struct sockaddr_in server;
-     struct sockaddr_in from;
-     char buf[1024];
-     fd_set readfds;
+   int portno2 =0; // default value;
+   int portno3 = 0;
+   //sockfd and newsockfd are file descriptors, i.e. array subscripts into the file descriptor table . 
+   // These these "sock variables" store the values returned by the socket system call and the accept system call.
+   // the "portno variables"  stores the port number/s on which the server accepts connections
+   // n is the return value for the read() and write() calls; i.e. it contains the number of characters read or written.
+     socklen_t clilen; // clilen stores the size of the address of the client. This is needed for the accept system call.
+     char buffer[256]; // The server reads characters from the socket connection into this buffer. May store message
+     struct sockaddr_in serv_addr, cli_addr; // Make multiple of them to accept multiple port numbers
+   struct sockaddr_in serv_addr2; // for port number 2
+   struct sockaddr_in serv_addr3; // for port number 3
+     socklen_t fromlen; //???
+     // // A sockaddr_in is a structure containing an internet address. This structure is defined in netinet/in.h.
+      struct sockaddr_in server; // should this be serv_addr?
+     struct sockaddr_in from; // should this be cli_addr?
+   
+   // The variable serv_addr(serverr) will contain the address of the server, and cli_addr(from) will contain the address of the client which connects to the server.
+
+
+     char buf[1024]; // The server reads characters from the socket connection into this buffer. May store message
+     fd_set readfds; // ???
 
      // outputs an error if no port is provided
      if (argc < 2){
@@ -38,30 +51,95 @@ int main(int argc, char *argv[])
          exit(1);
      }
 
-     // creates a new TCP and UDP socket
+     // creates a new TCP and UDP socket. I assume input validation is happening in those functions
      sockfd = makeSocket(SOCK_STREAM);
      sockfd_udp = makeSocket(SOCK_DGRAM);
 
      // sets the values in serv_addr
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+   //The function bzero() sets all values in a buffer to zero. It takes two arguments, 
+   //the first is a pointer to the buffer and the second is the size of the buffer. Thus, this line initializes serv_addr to zeros.
+     bzero((char *) &serv_addr, sizeof(serv_addr)); // when does serv_addr store the buffer? 
+     
+    // Getting port numbers
+   //The port number on which the server will listen for connections is passed in as an argument,
+   //and this statement uses the atoi() function to convert this from a string of digits to an integer.
+   // “echo_s” should	reply	to	any	client client	connected	to	one	of	its	ports	with	the	same	
+   // message	it	receives.
+   portno = atoi(argv[1]); // How do you know if this port number is the same or from the client port number
+      
+   if(argc > 2)
+     {
+        portno2 = atoi(argv[2]);
+     } 
+        if(argc > 3)
+     {
+        portno3 = atoi(argv[3]);
+     }
 
+   
+   //The variable serv_addr is a structure of type struct sockaddr_in. This structure has four fields. The first field is 
+   //short sin_family, which contains a code for the address family. It should always be set to the symbolic constant AF_INET.
+     serv_addr.sin_family = AF_INET;
+  
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+   //The third field of sockaddr_in is a structure of type struct in_addr which contains only a single field unsigned long s_addr. 
+   // This field contains the IP address of the host. For server code, this will 
+   // always be the IP address of the machine on which the server is running, and there is a 
+   // symbolic constant INADDR_ANY which gets this address.
+   
+   // How to get IP addresses of machine where client is running rather than the the machine the server is running?
+     
+   serv_addr.sin_port = htons(portno); // assigns serv_addr the port number?
+   // The second field of serv_addr is unsigned short sin_port, which contain the port number. However, instead of simply copying 
+   // the port number to this field, it is necessary to convert this 
+   // to network byte order using the function htons() which converts a port number in host byte order to a port number 
+   // in network byte order.
+
+   // Setting up for port 2
+        serv_addr2.sin_family = AF_INET; 
+  
+     serv_addr2.sin_addr.s_addr = INADDR_ANY;
+  
+     
+   serv_addr2.sin_port = htons(portno2); 
+   
+      serv_addr3.sin_family = AF_INET; 
+  
+     serv_addr3.sin_addr.s_addr = INADDR_ANY;
+   
+      // Setting up for port 3
+        serv_addr2.sin_family = AF_INET; 
+  
+     serv_addr2.sin_addr.s_addr = INADDR_ANY;
+  
+     
+   serv_addr2.sin_port = htons(portno2); 
+   
+      serv_addr3.sin_family = AF_INET; 
+  
+     serv_addr3.sin_addr.s_addr = INADDR_ANY;
+  
+     
+   serv_addr3.sin_port = htons(portno3); 
      // binds each socket to the port and outputs an error if it fails
      if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
          error("ERROR on binding");
      if (bind(sockfd_udp, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
          error("ERROR on binding");
+   
+   //Bind the socket to an address using the bind() system call. 
+   //For a server socket on the Internet, an address consists of a port number on the host machine.
 
      // listens to each socket and outputs an error if it fails
      listen(sockfd,5);
-    int  clilen = sizeof(cli_addr);
-     listen(sockfd_udp,5);
-    int  clilen2 = sizeof(cli_addr);
+    int  clilen = sizeof(cli_addr); // stores the size of the address of the client
+     listen(sockfd_udp,5); 
+    int  clilen2 = sizeof(cli_addr); // I do not know the use of this?
+   
+   // What if multiple clients connect to the server? Can make multiple proceeses to handle this?
 
      // clear the set
+   // Why clear the set? What does it even do? 
 
 FD_ZERO(&readfds);
 
@@ -89,7 +167,7 @@ FD_ZERO(&readfds);
 
             // receives a message from the client and places it in buffer, outputs an error if it fails
             bzero(buffer,256);
-            n = read(newsockfd,buffer,255);
+            n = read(newsockfd,buffer,255); // but sockfd not explicity assigned message from client
             if (n < 0)
                error("ERROR reading from socket");
 
@@ -124,6 +202,7 @@ FD_ZERO(&readfds);
          // sockfd is read ready
          if(FD_ISSET(sockfd, &readfds))
          {
+            // This call typically blocks until a client connects with the server.
             // accepts the TCP socket and outputs an error if accept fails
             newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
             if (newsockfd < 0)
@@ -236,3 +315,26 @@ FD_ZERO(&readfds);
      return 0;
 }
 }
+    // https://www.cs.bu.edu/teaching/c/file-io/intro/
+    /*FILE *ifp, *ofp;
+   char *mode = "r";
+   char outputFilename[] = "echo.list";
+
+   ifp = fopen("in.list", mode);
+
+   if (ifp == NULL) {
+     fprintf(stderr, "Can't open input file in.list!\n");
+     exit(1);
+   }
+
+   ofp = fopen(outputFilename, "w");
+
+   if (ofp == NULL) {
+     fprintf(stderr, "Can't open output file %s!\n",
+             outputFilename);
+     exit(1);
+   }
+   // Once a file has been successfully opened, you can read from it using fscanf() or write to it using fprintf();
+   fclose(ifp);
+fclose(ofp);
+        
