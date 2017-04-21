3d28dd2672cd5af66010dbcf73536fb7d3a87b5c
JacobFact <42regime@gmail.com>
2017-04-21 18:34:14 -0500

    Update client_anvitha.c

--- client_anvitha.c
+++ client_anvitha.c
@@ -14,76 +14,71 @@ printf("UDP(1) or TCP(2): ");
 scanf("%d", &answerstr);
 if(answerstr==2)
 {
-int sockfd, portno, n;
-struct sockaddr_in serv_addr;
-struct hostent *server;
-char buffer[256];
-if (argc < 3) {
-fprintf(stderr,"usage %s hostname port\n", argv[0]);
-exit(0);
-}
-portno = atoi(argv[2]);
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
-if (sockfd < 0) 
-error("ERROR opening socket");
-server = gethostbyname(argv[1]);
-if (server == NULL) {
-fprintf(stderr,"ERROR, no such host\n");
-exit(0);
-}
-bzero((char *) &serv_addr, sizeof(serv_addr));
-serv_addr.sin_family = AF_INET;
-bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,
-server->h_length);
-serv_addr.sin_port = htons(portno);
-if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-error("ERROR connecting");
-printf("Please enter the message: ");
-bzero(buffer,256);
-fgets(buffer,255,stdin);
-n = write(sockfd,buffer,strlen(buffer));
-if (n < 0) 
-error("ERROR writing to socket");
-bzero(buffer,256);
-n = read(sockfd,buffer,255);
-if (n < 0) 
-error("ERROR reading from socket");
-printf("%s\n",buffer);
-close(sockfd);
-return 0;
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
 }
 else if(answerstr==1)
 {
-int sock, n;
-unsigned int length;
-struct sockaddr_in server, from;
-struct hostent *hp;
-char buffer[256];
-if (argc != 3) {
-printf("Usage: server port\n");
-exit(1);
-}
-sock= makeSocket(SOCK_DGRAM);
-server.sin_family = AF_INET;
-hp = gethostbyname(argv[1]);
-if (hp==0) error("Unknown host");
-bcopy((char *)hp->h_addr, 
-(char *)&server.sin_addr,
-hp->h_length);
-server.sin_port = htons(atoi(argv[2]));
-length=sizeof(struct sockaddr_in);
-printf("Please enter the message: ");
-bzero(buffer,256);
-fgets(buffer,255,stdin);
-n=sendto(sock,buffer,strlen(buffer),0,(const struct sockaddr *)&server,length);
-if (n < 0) 
-n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
-if (n < 0)
-error("recvfrom");
-write(1,"Got an ack: ",12);
-write(1,buffer,n);
-close(sock);
-return 0;
+   int sockfd, servlen,n;
+   struct sockaddr_un  serv_addr;
+   char buffer[82];
+
+   bzero((char *)&serv_addr,sizeof(serv_addr));
+   serv_addr.sun_family = AF_UNIX;
+   strcpy(serv_addr.sun_path, argv[1]);
+   servlen = strlen(serv_addr.sun_path) + 
+                 sizeof(serv_addr.sun_family);
+   if ((sockfd = socket(AF_UNIX, SOCK_STREAM,0)) < 0)
+       error("Creating socket");
+   if (connect(sockfd, (struct sockaddr *) 
+                         &serv_addr, servlen) < 0)
+       error("Connecting");
+   printf("Please enter your message: ");
+   bzero(buffer,82);
+   fgets(buffer,80,stdin);
+   write(sockfd,buffer,strlen(buffer));
+   n=read(sockfd,buffer,80);
+   printf("The return message was\n");
+   write(1,buffer,n);
+   close(sockfd);
+   return 0;
 }
 else
 {
