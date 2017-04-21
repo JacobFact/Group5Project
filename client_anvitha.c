fd04afc17d80cfb4a1e64ca8edccd20bf054ab1b
JacobFact <42regime@gmail.com>
2017-04-21 18:17:52 -0500

    Update client_anvitha.c

--- client_anvitha.c
+++ client_anvitha.c
@@ -38,7 +38,7 @@ bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,
 server->h_length);
 serv_addr.sin_port = htons(portno);
 if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-error("ERROR connecting”);
+error("ERROR connecting");
 printf("Please enter the message: ");
 bzero(buffer,256);
 fgets(buffer,255,stdin);
@@ -48,7 +48,7 @@ error("ERROR writing to socket");
 bzero(buffer,256);
 n = read(sockfd,buffer,255);
 if (n < 0) 
-error("ERROR reading from socket”);
+error("ERROR reading from socket");
 printf("%s\n",buffer);
 close(sockfd);
 return 0;
